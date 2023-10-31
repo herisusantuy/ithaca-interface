058491ba07890977bbfeae10e233fc08aa712fbd
devVenus1202 <venus1202winter@gmail.com>
2023-10-31 15:35:30 -0400

    add gradient to fill chat

--- src/UI/components/ChartPayoff/ChartPayoff.tsx
+++ src/UI/components/ChartPayoff/ChartPayoff.tsx
@@ -54,17 +54,18 @@ const ChartPayoff = (props: Prop) => {
   return (
     <AreaChart width={400} height={300} data={modifiedData}>
       <defs>
-        <linearGradient id='splitColor' gradientTransform="rotate(90)">
+        <linearGradient id='splitColor' gradientTransform='rotate(90)'>
           <stop offset={off} stopColor='#5ee192' stopOpacity={0.2} />
           <stop offset={off} stopColor='#FF3F57' stopOpacity={0.2} />
         </linearGradient>
+        <linearGradient id='fillGradient' x1='0' y1='0' x2='0' y2='1'>
+          <stop offset='5%' stopColor='#8884d8' stopOpacity={0.4} />
+          <stop offset='50%' stopColor='#8884d8' stopOpacity={0} />
+          <stop offset='95%' stopColor='#8884d8' stopOpacity={0.4} />
+        </linearGradient>
       </defs>
       <Tooltip />
-      <Area
-        type='monotone'
-        dataKey='value'
-        fill='url(#splitColor)'
-      />
+      <Area type='linear' stroke='#8884d8' dataKey='value' fill='url(#fillGradient)' />
       <ReferenceLine y={0} stroke='red' />
     </AreaChart>
   );
