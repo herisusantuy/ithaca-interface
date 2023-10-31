6c580645ec64164cfa634ad4da6fcb4e3c56aaa1
Kyle Williams <me@kylewilliams.co.uk>
2023-10-31 21:03:09 +0800

    Implement input component

---
+++ src/UI/components/Input/Input.tsx
@@ -0,0 +1,36 @@
+// Packages
+import { ChangeEvent, ReactNode } from 'react';
+
+// Utils
+import { preventScrollOnNumberInput } from '@/UI/utils/Input';
+
+// Types
+type Props = {
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  value?: number;
+  icon?: ReactNode;
+  placeholder?: string;
+  disabled?: boolean;
+};
+
+// Styles
+import styles from './Input.module.scss';
+
+const Input = ({ onChange, value, icon, disabled, placeholder = '0' }: Props) => {
+  return (
+    <div className={styles.input}>
+      <input
+        type='number'
+        value={value}
+        placeholder={placeholder}
+        inputMode='decimal'
+        onChange={onChange}
+        onWheel={preventScrollOnNumberInput}
+        disabled={disabled}
+      />
+      {icon && icon}
+    </div>
+  );
+};
+
+export default Input;
