ac7a15de9a82871ee7fad766b83e59332dc8c53b
Kyle Williams <me@kylewilliams.co.uk>
2023-10-31 16:09:46 +0800

    Fix sidebar overflow

--- src/UI/utils/ChartMapper.tsx
+++ src/UI/utils/ChartMapper.tsx
@@ -1,9 +1,10 @@
 // Components
 import Chart from '@/UI/components/Chart/Chart';
+import ChartPayoff from '@/UI/components/ChartPayoff/ChartPayoff';
 
 // Map charts to contentId from TRADING_MARKET_TABS constants
 const contentMap: { [key: string]: JSX.Element } = {
-  optionsChart: <Chart />,
+  optionsChart: <ChartPayoff />,
   digitalOptionsChart: <Chart />,
   forwardsChart: <Chart />,
   optionsCall: <Chart />,
