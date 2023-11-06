8cb40a252fdf930c157146d67aefc504e71db815
devVenus1202 <venus1202winter@gmail.com>
2023-11-07 01:59:28 +0400

    feat(:update)Optimize code for modalwithbutton

--- src/UI/components/Modal/ModalWithButton.tsx
+++ src/UI/components/Modal/ModalWithButton.tsx
@@ -1,6 +1,9 @@
-import { ReactNode, useState } from "react";
-import Button from "../Button/Button";
-import Modal from "./Modal";
+// Package import
+import { ReactNode, useState } from 'react';
+
+// Components import
+import Button from '@/UI/components/Button/Button';
+import Modal from '@/UI/components/Modal/Modal';
 
 type ModalButtonProps = {
   title: string;
@@ -9,23 +12,33 @@ type ModalButtonProps = {
   isLoading: boolean;
   onSubmitOrder: () => void;
 };
+
 const ModalWithButton = (props: ModalButtonProps) => {
-  const {isLoading, onSubmitOrder} = props;
+  const { isLoading, onSubmitOrder } = props;
   const [showModal, setShowModal] = useState<boolean>(false);
   const handleOnClick = () => {
     setShowModal(!showModal);
-  }
+  };
   const handleSubmit = () => {
     onSubmitOrder();
     setShowModal(false);
-  }
+  };
   return (
     <>
-      <Button title={props.title} onClick={handleOnClick}>{props.btnText}</Button>
-      <Modal title='Manage Funds' isOpen={showModal} isLoading={isLoading} onCloseModal={() => setShowModal(false)} onSubmitOrder={handleSubmit}>
+      <Button title={props.title} onClick={handleOnClick}>
+        {props.btnText}
+      </Button>
+      <Modal
+        title='Manage Funds'
+        isOpen={showModal}
+        isLoading={isLoading}
+        onCloseModal={() => setShowModal(false)}
+        onSubmitOrder={handleSubmit}
+      >
         {props.children}
       </Modal>
     </>
-  )
-}
+  );
+};
+
 export default ModalWithButton;
