1b5dd4958855556343b108e24e9143ac8345a4d5
Kyle Williams <me@kylewilliams.co.uk>
2023-11-07 21:14:47 +0800

    Update dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,20 +1,58 @@
+// Packages
+import dynamic from 'next/dynamic';
+
 // Components
 import Meta from '@/UI/components/Meta/Meta';
+
+// Constants
+import { TABLE_ORDER_DATA_WITH_EXPANDED } from '@/UI/constants/tableOrder';
+
+// Components
+import CollateralPanel from '@/UI/components/CollateralPanel/CollateralPanel';
+import Tabs from '@/UI/components/Tabs/Tabs';
+
+const TableOrder = dynamic(() => import('@/UI/components/TableOrder/TableOrder'), {
+  ssr: false,
+});
 
 // Layout
 import Main from '@/UI/layouts/Main/Main';
 import Container from '@/UI/layouts/Container/Container';
-import CollateralPanel from '@/UI/components/CollateralPanel/CollateralPanel';
-// import OrderPanel from '@/UI/components/OrderPanel/OrderPanel';
+import Panel from '@/UI/layouts/Panel/Panel';
 
 const Dashboard = () => {
+  const DASHBOARD_TABS = [
+    {
+      id: 'liveOrders',
+      label: 'Live Orders',
+      content: <TableOrder data={TABLE_ORDER_DATA_WITH_EXPANDED} />,
+    },
+    {
+      id: 'positions',
+      label: 'Positions',
+      content: <p>Coming soon.</p>,
+    },
+    {
+      id: 'tradeHistory',
+      label: 'Trade History',
+      content: <p>Coming soon.</p>,
+    },
+    {
+      id: 'fundLockHistory',
+      label: 'Fund Lock History',
+      content: <p>Coming soon.</p>,
+    },
+  ];
+
   return (
     <>
       <Meta />
       <Main>
         <Container>
-          <CollateralPanel/>
-          {/* <OrderPanel /> */}
+          <CollateralPanel />
+          <Panel margin='p-30 mt-15'>
+            <Tabs tabs={DASHBOARD_TABS} />
+          </Panel>
         </Container>
       </Main>
     </>
