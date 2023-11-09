f9fa4bb1dc3b1db3ab340f6c7e17c89afcc9b746
Kyle Williams <me@kylewilliams.co.uk>
2023-11-09 12:01:06 +0800

    Updates to table functionality

--- src/UI/components/Pagination/Pagination.tsx
+++ src/UI/components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 // Components
 import Next from '@/UI/components/Icons/Next';
+import Previous from '@/UI/components/Icons/Previous';
 
 // Styles
 import styles from './Pagination.module.scss';
@@ -12,199 +13,72 @@ type PaginationProps = {
   onPageChange: (page: number) => void;
 };
 
+type NavigationItemProps = {
+  page: number;
+  isCurrent?: boolean;
+  isDisabled?: boolean;
+};
+
 const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }: PaginationProps) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-  // const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  const updatePage = (page: number) => {
-    if (page > totalPages) {
-      onPageChange(currentPage);
-    } else {
-      onPageChange(page);
-    }
+  const updatePage = (newPage: number) => {
+    const page = newPage > totalPages ? currentPage : newPage;
+    onPageChange(page);
   };
 
-  const pageNavigationRender = () => {
+  const NavigationItem = ({ page, isCurrent = false, isDisabled = false }: NavigationItemProps) => (
+    <div
+      className={`${styles.navigationItem} ${isCurrent ? styles.active : ''} ${isDisabled ? styles.disabled : ''}`}
+      onClick={() => !isDisabled && updatePage(page)}
+    >
+      {page}
+    </div>
+  );
+
+  const renderPageNumbers = () => {
+    const items = [];
+    let startPage = currentPage - 2;
+    let endPage = currentPage + 2;
+
     if (currentPage <= 5) {
-      return (
-        <>
-          <div
-            className={`${styles.navigationItem} ${currentPage == 1 ? styles.active : ''}`}
-            onClick={() => updatePage(1)}
-          >
-            1
-          </div>
-          <div
-            className={`${styles.navigationItem} ${currentPage == 2 ? styles.active : ''} ${
-              totalPages < 2 ? styles.disabled : ''
-            }`}
-            onClick={() => updatePage(2)}
-          >
-            2
-          </div>
-          <div
-            className={`${styles.navigationItem} ${currentPage == 3 ? styles.active : ''} ${
-              totalPages < 3 ? styles.disabled : ''
-            }`}
-            onClick={() => updatePage(3)}
-          >
-            3
-          </div>
-          <div
-            className={`${styles.navigationItem} ${currentPage == 4 ? styles.active : ''} ${
-              totalPages < 4 ? styles.disabled : ''
-            }`}
-            onClick={() => updatePage(4)}
-          >
-            4
-          </div>
-          <div
-            className={`${styles.navigationItem} ${currentPage == 5 ? styles.active : ''} ${
-              totalPages < 5 ? styles.disabled : ''
-            }`}
-            onClick={() => updatePage(5)}
-          >
-            5
-          </div>
-          <div
-            className={`${styles.navigationItem} ${totalPages < currentPage + 1 ? styles.disabled : ''} ${
-              styles.nextItem
-            }`}
-            onClick={() => updatePage(currentPage + 1)}
-          >
-            <Next />
-          </div>
-        </>
-      );
+      startPage = 1;
+      endPage = 5;
+    } else if (currentPage > totalPages - 5) {
+      startPage = totalPages - 4;
+      endPage = totalPages;
     }
 
-    if (currentPage > 5) {
-      if (currentPage <= totalPages - 2) {
-        return (
-          <>
-            <div
-              className={`${styles.navigationItem} ${styles.previewItem}`}
-              onClick={() => updatePage(currentPage - 1)}
-            >
-              <Next />
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 2)}>
-              {currentPage - 2}
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 1)}>
-              {currentPage - 1}
-            </div>
-
-            <div className={`${styles.navigationItem} ${styles.active}`} onClick={() => updatePage(currentPage)}>
-              {currentPage}
-            </div>
-            <div
-              className={`${styles.navigationItem} ${currentPage + 1 > totalPages ? styles.disabled : ''}`}
-              onClick={() => updatePage(currentPage + 1)}
-            >
-              {currentPage + 1}
-            </div>
-            <div
-              className={`${styles.navigationItem} ${currentPage + 2 > totalPages ? styles.disabled : ''}`}
-              onClick={() => updatePage(currentPage + 2)}
-            >
-              {currentPage + 2}
-            </div>
-            <div
-              className={`${styles.navigationItem} ${currentPage >= totalPages ? styles.disabled : ''} ${
-                styles.nextItem
-              }`}
-              onClick={() => updatePage(currentPage + 1)}
-            >
-              <Next />
-            </div>
-          </>
-        );
-      }
-      if (currentPage <= totalPages - 1) {
-        return (
-          <>
-            <div
-              className={`${styles.navigationItem} ${styles.previewItem}`}
-              onClick={() => updatePage(currentPage - 1)}
-            >
-              <Next />
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 3)}>
-              {currentPage - 3}
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 2)}>
-              {currentPage - 2}
-            </div>
-
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 1)}>
-              {currentPage - 1}
-            </div>
-            <div className={`${styles.navigationItem} ${styles.active}`} onClick={() => updatePage(currentPage)}>
-              {currentPage}
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage + 1)}>
-              {currentPage + 1}
-            </div>
-            <div
-              className={`${styles.navigationItem} ${currentPage >= totalPages ? styles.hide : ''} ${styles.nextItem}`}
-              onClick={() => updatePage(currentPage + 1)}
-            >
-              <Next />
-            </div>
-          </>
-        );
-      }
-      if (currentPage == totalPages) {
-        return (
-          <>
-            <div
-              className={`${styles.navigationItem} ${styles.previewItem}`}
-              onClick={() => updatePage(currentPage - 1)}
-            >
-              <Next />
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 4)}>
-              {currentPage - 4}
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 3)}>
-              {currentPage - 3}
-            </div>
-
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 2)}>
-              {currentPage - 2}
-            </div>
-            <div className={`${styles.navigationItem}`} onClick={() => updatePage(currentPage - 1)}>
-              {currentPage - 1}
-            </div>
-            <div className={`${styles.navigationItem} ${styles.active}`} onClick={() => updatePage(currentPage)}>
-              {currentPage}
-            </div>
-            <div
-              className={`${styles.navigationItem} ${currentPage >= totalPages ? styles.hide : ''} ${styles.nextItem}`}
-              onClick={() => updatePage(currentPage + 1)}
-            >
-              <Next />
-            </div>
-          </>
+    for (let page = startPage; page <= endPage; page++) {
+      if (page > 0 && page <= totalPages) {
+        items.push(
+          <NavigationItem key={page} page={page} isCurrent={currentPage === page} isDisabled={page > totalPages} />
         );
       }
     }
+
+    return items;
   };
+
+  const renderPreviousButton = () =>
+    currentPage > 1 && (
+      <div className={`${styles.navigationItem} ${styles.nextItem}`} onClick={() => updatePage(currentPage - 1)}>
+        <Previous />
+      </div>
+    );
+
+  const renderNextButton = () =>
+    currentPage < totalPages && (
+      <div className={`${styles.navigationItem} ${styles.nextItem}`} onClick={() => updatePage(currentPage + 1)}>
+        <Next />
+      </div>
+    );
 
   return (
     <div className={styles.pagination}>
-      {/* {pages.map(page => (
-        <Button
-          key={page}
-          disabled={currentPage === page}
-          onClick={() => onPageChange(page)}
-          title='Click to view next page'
-        >
-          {page}
-        </Button>
-      ))} */}
-      {pageNavigationRender()}
+      {renderPreviousButton()}
+      {renderPageNumbers()}
+      {renderNextButton()}
     </div>
   );
 };
