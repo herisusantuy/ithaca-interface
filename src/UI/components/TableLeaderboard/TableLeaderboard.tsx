2f46537315e8f4badda1eac3aa79ddcfec9baa65
Kyle Williams <me@kylewilliams.co.uk>
2023-11-14 13:11:56 +0800

    Add sort to leaderboard table

--- src/UI/components/TableLeaderboard/TableLeaderboard.tsx
+++ src/UI/components/TableLeaderboard/TableLeaderboard.tsx
@@ -1,5 +1,5 @@
 // Packages
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 // Constants
 import { LeaderboardEntry, TABLE_LEADERBOARD_HEADERS } from '@/UI/constants/tableLeaderboard';
@@ -10,6 +10,8 @@ import Flex from '@/UI/layouts/Flex/Flex';
 // Components
 import Pagination from '@/UI/components/Pagination/Pagination';
 import Avatar from '@/UI/components/Icons/Avatar';
+import Button from '@/UI/components/Button/Button';
+import Sort from '@/UI/components/Icons/Sort';
 
 // Styles
 import styles from './TableLeaderboard.module.scss';
@@ -19,8 +21,25 @@ type TableLeaderboardProps = {
   data: LeaderboardEntry[];
 };
 
+type SortConfig = {
+  key: keyof LeaderboardEntry;
+  direction: 'ascending' | 'descending';
+};
+
 const TableLeaderboard = ({ data }: TableLeaderboardProps) => {
+  const initialSortConfig: SortConfig = {
+    key: 'ranking',
+    direction: 'ascending',
+  };
+
+  const headerToKeyMap: Record<string, keyof LeaderboardEntry> = {
+    Ranking: 'ranking',
+    '24H Points': 'points',
+    'Total Points': 'totalPoints',
+  };
+
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortConfig, setSortConfig] = useState<SortConfig | null>(initialSortConfig);
 
   const pageLimit = 9;
 
@@ -29,19 +48,55 @@ const TableLeaderboard = ({ data }: TableLeaderboardProps) => {
     setCurrentPage(page);
   };
 
+  // Sort the data
+  const sortableColumns = ['Ranking', '24H Points', 'Total Points'];
+
+  const sortedData = useMemo(() => {
+    const sortableItems = [...data];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [data, sortConfig]);
+
+  // Handle sorting
+  const requestSort = (key: keyof LeaderboardEntry) => {
+    let direction: 'ascending' | 'descending' = 'ascending';
+    if (sortConfig && sortConfig.key === key && sortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
   return (
     <div className={styles.table}>
       <div className={styles.header}>
         {TABLE_LEADERBOARD_HEADERS.map((header, idx) => {
+          const isSortable = sortableColumns.includes(header);
+          const sortKey = headerToKeyMap[header];
+
           return (
             <div className={styles.cell} key={idx}>
               {header}
+              {isSortable && (
+                <Button title='Click to sort column' className={styles.sort} onClick={() => requestSort(sortKey)}>
+                  <Sort />
+                </Button>
+              )}
             </div>
           );
         })}
       </div>
-      {data.length ? (
-        data.map((leader, idx) => (
+      {sortedData.length ? (
+        sortedData.map((leader, idx) => (
           <div className={styles.row} key={idx}>
             <div className={styles.cell}>{leader.ranking}</div>
             <div className={styles.cell}>
@@ -57,9 +112,9 @@ const TableLeaderboard = ({ data }: TableLeaderboardProps) => {
           <div className={styles.row}>No results found</div>
         </div>
       )}
-      {data.length > 9 ? (
+      {sortedData.length > 9 ? (
         <Flex direction='row-space-between' margin='mt-35'>
-          <div></div>
+          <div />
           <Pagination
             totalItems={data.length}
             itemsPerPage={pageLimit}
