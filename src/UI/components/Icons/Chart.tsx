d0ed45a07314df4ce94da1d94f0f570956f6910d
Kyle Williams <me@kylewilliams.co.uk>
2023-11-16 09:41:54 +0800

    Add placeholder for no chart data

---
+++ src/UI/components/Icons/Chart.tsx
@@ -0,0 +1,16 @@
+const Chart = () => {
+  return (
+    <svg width='46' height='47' viewBox='0 0 46 47' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <rect x='5.75' y='6.46582' width='34.5' height='34.5' rx='2' fill='#35333E' />
+      <path
+        d='M34.5 16.0518L26.938 25.5042C26.8822 25.574 26.778 25.5798 26.7148 25.5166L21.1995 20.0012C21.1371 19.9389 21.0346 19.9435 20.9782 20.0113L11.5 31.3851'
+        stroke='white'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default Chart;
