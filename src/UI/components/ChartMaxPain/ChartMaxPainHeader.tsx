c7c7f12ad09473aa8d50b6632e2d77765e3efaf5
devVenus1202 <venus1202winter@gmail.com>
2023-11-17 07:02:30 +0400

    feat(:add)Add MaxPain header part

--- src/UI/components/ChartMaxPain/ChartMaxPainHeader.tsx
+++ src/UI/components/ChartMaxPain/ChartMaxPainHeader.tsx
@@ -1,4 +1,4 @@
-import DropdownMenu from '../DropdownMenu/DropdownMenu';
+import DropdownMenu, { DropDownOption } from '../DropdownMenu/DropdownMenu';
 
 // import constants
 import { DROPDOWN_DATE_OPTIONS } from '@/UI/constants/dropdown';
@@ -6,18 +6,33 @@ import { DROPDOWN_DATE_OPTIONS } from '@/UI/constants/dropdown';
 // Import styles
 import styles from './ChartMaxPain.module.scss';
 import LogoEth from '../Icons/LogoEth';
+import { useState } from 'react';
 
 const ChartMaxPainHeader = () => {
+  const [option, setOption] = useState(DROPDOWN_DATE_OPTIONS[0]['value']);
+  const [selectedOption, setSlectedOption] = useState<DropDownOption>(DROPDOWN_DATE_OPTIONS[0]);
+
+  const selectOption = (value: string, selectedOption: DropDownOption) => {
+    setOption(value);
+    setSlectedOption(selectedOption);
+  };
+
   return (
     <>
       <div className={styles.headerContainer}>
         <div className={styles.headerExpiryContainer}>
           <p>Expiry Date</p>
-          <DropdownMenu options={DROPDOWN_DATE_OPTIONS} onChange={() => {}} />
+          <DropdownMenu
+            options={DROPDOWN_DATE_OPTIONS}
+            onChange={(value, selectedOption) => {
+              selectOption(value, selectedOption);
+            }}
+            value={selectedOption}
+          />
         </div>
         <div className={styles.headerContent}>
           <p>The Max Pain for</p>
-          <LogoEth /> <h3>WETH</h3> <p>options expiring on</p> <h3>10Nov23</h3> <p>is</p> <h3>1600</h3> <LogoEth />
+          <LogoEth /> <h3>WETH</h3> <p>options expiring on</p> <h3>{option}</h3> <p>is</p> <h3>1600</h3> <LogoEth />
           <p>WETH</p>
         </div>
       </div>
