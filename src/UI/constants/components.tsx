df8f3e97c34a9c8648120fbd5fc879d984cbdf79
devVenus1202 <venus1202winter@gmail.com>
2023-11-20 13:58:01 +0400

    feat(:add)Add datepicker

--- src/UI/constants/components.tsx
+++ src/UI/constants/components.tsx
@@ -55,6 +55,7 @@ import ChartTradeCount from '@/UI/components/ChartTradeCount/ChartTradeCount';
 import Card from '@/UI/components/Card/Card';
 import TableLeaderboard from '@/UI/components/TableLeaderboard/TableLeaderboard';
 import ToastTest from '@/UI/components/Toast/ToastTest';
+import DatePicker from '@/UI/components/DatePicker/DatePicker';
 
 // Layouts
 import Container from '@/UI/layouts/Container/Container';
@@ -235,11 +236,11 @@ const COMPONENT_GROUPS = [
         component: <CurrencyDisplay amount={10} symbol={<LogoUsdc />} currency='USDC' />,
         status: 'Approved',
       },
-      // {
-      //   name: 'DatePicker',
-      //   component: <DatePicker />,
-      //   status: 'In Progress',
-      // },
+      {
+        name: 'DatePicker',
+        component: <DatePicker />,
+        status: 'In Progress',
+      },
       {
         name: 'DisconnectedWallet',
         component: (
