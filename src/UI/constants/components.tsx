bbb3cb8bad90793a3bab3c0f9322f81d1c79e41a
Kyle Williams <me@kylewilliams.co.uk>
2023-11-20 11:28:00 +0800

    Fix pipeline

--- src/UI/constants/components.tsx
+++ src/UI/constants/components.tsx
@@ -55,7 +55,6 @@ import ChartTradeCount from '@/UI/components/ChartTradeCount/ChartTradeCount';
 import Card from '@/UI/components/Card/Card';
 import TableLeaderboard from '@/UI/components/TableLeaderboard/TableLeaderboard';
 import ToastTest from '@/UI/components/Toast/ToastTest';
-import Datepicker from '@/UI/components/DatePicker/Datepicker';
 
 // Layouts
 import Container from '@/UI/layouts/Container/Container';
@@ -77,6 +76,7 @@ import { CHART_OPEN_INTEREST_DATA } from './charts/chartOpenInterest';
 import { CHART_MAX_PAIN_ANY_TYPE } from './charts/chartMaxPain';
 import { LEADERBOARD_CARDS } from './leaderboard';
 import { TABLE_LEADERBOARD_DATA } from './tableLeaderboard';
+import Datepicker from '../components/DatePicker/Datepicker';
 
 const COMPONENT_GROUPS = [
   {
