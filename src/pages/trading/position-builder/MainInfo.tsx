f9e15ad3ac79b3bf3c7f2aa7899145f1b698848f
imchristiandev <imchristiandev@gmail.com>
2023-12-05 13:01:39 -0500

    feat: creation of currency component

--- src/pages/trading/position-builder/MainInfo.tsx
+++ src/pages/trading/position-builder/MainInfo.tsx
@@ -1,16 +1,10 @@
 import { useState } from 'react'
 
-import Flex from "@/UI/layouts/Flex/Flex";
 import styles from './position-builder.module.scss'
-import LogoEth from "@/UI/components/Icons/LogoEth";
-import Asset from "@/UI/components/Asset/Asset";
-import LabelValue from "@/UI/components/LabelValue/LabelValue";
 import { useAppStore } from '@/UI/lib/zustand/store';
-import { getNumber } from '@/UI/utils/Numbers';
 import { Leg } from '@ithaca-finance/sdk';
 
 import dayjs from 'dayjs';
-import CountdownTimer from "@/UI/components/CountdownTimer/CountdownTimer";
 import PositionBuilderRow from "@/UI/components/PositionBuilderRow/PositionBuilderRow";
 import RadioButton from "@/UI/components/RadioButton/RadioButton";
 import { useDevice } from '@/UI/hooks/useDevice';
@@ -27,11 +21,9 @@ export interface PositionBuilderStrategy {
   strike: string;
 }
 export const MainInfo = ({
-  setOrderSummary, 
   positionBuilderStrategies, 
   setPositionBuilderStrategies, 
   getPositionBuilderSummary, 
-  setChartData 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 }: any) => {
   const [product, setProduct] = useState<string>('options')
@@ -49,7 +41,7 @@ export const MainInfo = ({
   const handleProductChange = (product: string) => { 
     setProduct(product)
   }
-  const { currentExpiryDate, expiryList, setCurrentExpiryDate } = useAppStore();
+  const { currentExpiryDate } = useAppStore();
 
   const handleAddStrategy = (strategy: PositionBuilderStrategy) => {
     const newPositionBuilderStrategies = [...positionBuilderStrategies, strategy];
@@ -138,45 +130,6 @@ export const MainInfo = ({
 
   return (
     <>
-      <Flex gap={ (device !== 'phone') ? 'gap-12' : 'gap-0' } margin='mb-24'>
-        <div className={styles.currency__info}>
-          <Asset icon={<LogoEth />} label='ETH' />
-        </div>
-        <div className={styles.currency__info}>
-          <LabelValue
-            label='Expiry Date'
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            valueList={expiryList.map((date:any) => ({
-              label: dayjs(`${date}`, 'YYYYMMDD').format('DD MMM YY'),
-              value: dayjs(`${date}`, 'YYYYMMDD').format('DD MMM YY'),
-            }))}
-            onChange={value => {
-              setOrderSummary(undefined);
-              setPositionBuilderStrategies([]);
-              setChartData(undefined);
-              setCurrentExpiryDate(getNumber(dayjs(value, 'DD MMM YY').format('YYYYMMDD')));
-            }}
-            value={dayjs(`${currentExpiryDate}`, 'YYYYMMDD').format('DD MMM YY')}
-            hasDropdown={true}
-          />
-        </div>
-        <div className={styles.currency__info}>
-          <LabelValue label='Next Auction' value={<CountdownTimer />} />
-        </div>
-        <div className={styles.currency__info}>
-          <LabelValue
-            label='Last Auction Price'
-            value='1629'
-            subValue={
-              <>
-                <span>{dayjs(`${currentExpiryDate}`, 'YYYYMMDD').format('DD')}</span>
-                <span>{dayjs(`${currentExpiryDate}`, 'YYYYMMDD').format('MMM')}</span>
-                <span>{dayjs(`${currentExpiryDate}`, 'YYYYMMDD').format('YY')}</span>
-              </>
-            }
-          />
-        </div>
-      </Flex>
       <h3>Position Builder</h3>
       { renderOptions() }
     </>
