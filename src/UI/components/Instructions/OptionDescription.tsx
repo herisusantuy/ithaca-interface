aa52dfa13d63c817c9edfb8065ed4f5df3ff82c7
Rob <bobs.evans@live.com>
2023-12-09 18:38:13 +0800

    fix: lint

--- src/UI/components/Instructions/OptionDescription.tsx
+++ src/UI/components/Instructions/OptionDescription.tsx
@@ -6,7 +6,7 @@ const OptionInstructions = () => {
   return (
     <div className={styles.container}>
       <p>
-      A Call Option is a contract allowing a user to buy an asset at a fixed price at contract expiry, while a Put Option provides the user with the right to sell. 
+      A Call Option is a contract allowing a user to buy an asset at a fixed price at contract expiry, while a Put Option provides the user with the right to sell.
       </p>
     </div>
   );
