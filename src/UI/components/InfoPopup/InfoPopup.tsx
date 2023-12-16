fd4bf2bc0525f5829d47b7b40e3b62eaf0319be7
Anton Perch <perch.anton@gmail.com>
2023-12-16 18:25:12 +0100

    fix: change hardcode date to dayjs from currentExpiryDate

--- src/UI/components/InfoPopup/InfoPopup.tsx
+++ src/UI/components/InfoPopup/InfoPopup.tsx
@@ -1,10 +1,17 @@
-import styles from './InfoPopup.module.scss';
+import dayjs from 'dayjs';
+
+import { useAppStore } from '@/UI/lib/zustand/store';
+
 import LogoEth from '@/UI/components/Icons/LogoEth';
 import ArrowRight from '@/UI/components/Icons/ArrowRight';
 import ChevronLeft from '@/UI/components/Icons/ChevronLeft';
 import ChevronRight from '@/UI/components/Icons/ChevronRight';
 
+import styles from './InfoPopup.module.scss';
+
 const InfoPopup = () => {
+  const { currentExpiryDate } = useAppStore();
+
   return (
     <div className={styles.popupContainer}>
       <p>
@@ -12,8 +19,7 @@ const InfoPopup = () => {
       </p>
       <p>
         <span className={styles.popupIfEth}>If</span>
-        <LogoEth />
-        <span className={styles.italic}>@ 20Nov23</span>
+        <LogoEth />@<span className={styles.italic}>{dayjs(currentExpiryDate).format('DD MMM YY')}</span>
         <ChevronRight />
         Barrier (<ChevronLeft />
         Strike ) <ArrowRight />
