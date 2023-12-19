bba05a1dbeb5499d11c466c51c419dd92e5372c5
Angelica Willianto <angelicawillianto@gmail.com>
2023-12-19 18:11:35 +0800

    fix: implement number formatting for collateral summary dashboard

--- src/UI/components/TableDescription/TableDescription.tsx
+++ src/UI/components/TableDescription/TableDescription.tsx
@@ -13,31 +13,81 @@ import { TableDescriptionProps } from '@/UI/constants/tableOrder';
 type ValueWithIcon = {
   value: number;
   Icon?: ComponentType;
+  type?: string;
 };
 
+const formatValue = (value: number, type?: string): string => {
+  switch (type) {
+    case 'eth':
+      if (value < 1e2) {
+        return value.toFixed(3);
+      } else if (value < 1e3) {
+        return value.toFixed(2);
+      } else if (value < 1e4) {
+        return value.toFixed(1);
+      } else if (value < 1e5) {
+        if (value % 1e3 === 0) {
+          return (value / 1e3).toFixed(0) + 'K';
+        } else {
+          return (value / 1e3).toFixed(1) + 'K';
+        }
+      } else if (value < 1e6) {
+        return (value / 1e3).toFixed(1) + 'K';
+      } else if (value < 1e7) {
+        return (value / 1e6).toFixed(3) + 'M';
+      } else if (value < 1e8) {
+        return (value / 1e6).toFixed(2) + 'M';
+      } else if (value < 1e9) {
+        return (value / 1e6).toFixed(1) + 'M';
+      } else {
+        return (value / 1e9).toFixed(3) + 'B';
+      }
+    default:
+      if (value < 1e3) {
+        return value.toFixed(2);
+      } else if (value < 1e4) {
+        return value.toFixed(1);
+      } else if (value < 1e5) {
+        if (value % 1e3 === 0) {
+          return value.toFixed(0);
+        } else {
+          return (value / 1e3).toFixed(1) + 'K';
+        }
+      } else if (value < 1e6) {
+        return (value / 1e3).toFixed(1) + 'K';
+      } else if (value < 1e7) {
+        return (value / 1e6).toFixed(3) + 'M';
+      } else if (value < 1e8) {
+        return (value / 1e6).toFixed(2) + 'M';
+      } else if (value < 1e9) {
+        return (value / 1e6).toFixed(1) + 'M';
+      } else {
+        return (value / 1e9).toFixed(3) + 'B';
+      }
+  }
+};
 
 const TableDescription = ({
   possibleReleaseX,
   possibleReleaseY,
   postOptimisationX,
-  postOptimisationY,
-  // totalCollateral,
+  postOptimisationY, // totalCollateral,
 }: TableDescriptionProps) => {
   const rows = [
     {
       label: 'Possible Collateral release: ',
       values: [
-        { value: possibleReleaseX, Icon: LogoEth },
-        { value: possibleReleaseY, Icon: LogoUsdc },
+        { value: possibleReleaseX, Icon: LogoEth, type: 'eth' },
+        { value: possibleReleaseY, Icon: LogoUsdc, type: 'usdc' },
       ] as ValueWithIcon[],
     },
     {
       label: 'Expected Collateral Value Post Execution Collateral Optimization: ',
       values: [
-        { value: postOptimisationX, Icon: LogoEth },
-        { value: postOptimisationY, Icon: LogoUsdc },
+        { value: postOptimisationX, Icon: LogoEth, type: 'eth' },
+        { value: postOptimisationY, Icon: LogoUsdc, type: 'usdc' },
       ] as ValueWithIcon[],
-    }
+    },
   ];
 
   return (
@@ -48,7 +98,7 @@ const TableDescription = ({
           {row.values.map((val, idx) => (
             <Fragment key={idx}>
               <span>
-                {val.value} {val.Icon && <val.Icon />}
+                {formatValue(val.value, val.type)} {val.Icon && <val.Icon />}
               </span>
               {idx < row.values.length - 1 && ', '}
             </Fragment>
