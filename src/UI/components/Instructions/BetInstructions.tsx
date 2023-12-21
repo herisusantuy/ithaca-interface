38dae4bf33c8505996bf5067e9eec8ca8ef2179a
dskoneczny <skoneczny.damian@gmail.com>
2023-12-21 11:09:50 +0100

    fix: Fix linting

--- src/UI/components/Instructions/BetInstructions.tsx
+++ src/UI/components/Instructions/BetInstructions.tsx
@@ -16,29 +16,34 @@ const BetInstructions = ({ type = 'INSIDE', currentExpiryDate }: BetInstructionT
   const renderCurrentExpiryDate = useMemo(() => {
     return (
       <>
-        @
-        <span className={`${styles.italic}  hide-psuedo p-0`}>
-          {dayjs(currentExpiryDate).format('DD MMM YY')}
-        </span>
+        @<span className={`${styles.italic}  hide-psuedo p-0`}>{dayjs(currentExpiryDate).format('DD MMM YY')}</span>
       </>
-    )
-  }, [currentExpiryDate])
-  
+    );
+  }, [currentExpiryDate]);
+
   return (
     <div className={styles.container}>
       <div className={styles.gridContainer}>
         <p>Bet & Earn Return if</p>
-        <p className='ml-6 mr-2'><LogoEth /></p>
+        <p className='ml-6 mr-2'>
+          <LogoEth />
+        </p>
         <p>{renderCurrentExpiryDate}</p>
-        <p className='ml-6'><InsideOutside type={type} /> Range.</p>
+        <p className='ml-6'>
+          <InsideOutside type={type} /> Range.
+        </p>
 
         <p>i. Bet Capital at Risk;</p>
-        <p className='ml-6'><LogoEth /></p>
+        <p className='ml-6'>
+          <LogoEth />
+        </p>
         <p>{renderCurrentExpiryDate}</p>
-        <p className='ml-6'><InsideOutside type={type} /> Range?</p>
+        <p className='ml-6'>
+          <InsideOutside type={type} /> Range?
+        </p>
       </div>
 
-      <p className="mb-10">ii. Select Range. </p>
+      <p className='mb-10'>ii. Select Range. </p>
       <p className='mb-4'>iii. Enter Target Earn.</p>
       <p>
         iv. Expected Return reflects the probability of
