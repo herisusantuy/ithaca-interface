c8452618de26954127707578833495cf99b29106
dskoneczny <skoneczny.damian@gmail.com>
2023-12-22 13:21:44 +0100

    feat: Update Deposit/Withdraw modal

--- src/UI/components/CollateralPanel/CollateralPanel.tsx
+++ src/UI/components/CollateralPanel/CollateralPanel.tsx
@@ -1,38 +1,26 @@
 // Packages
-import dynamic from 'next/dynamic';
 import { useAccount, useBalance, usePublicClient, useWalletClient } from 'wagmi';
+import { FetchBalanceResult } from '@wagmi/core';
 import { useCallback, useEffect, useState } from 'react';
 import { parseAbi, parseUnits } from 'viem';
 
 // Constants
 import { TABLE_COLLATERAL_SUMMARY } from '@/UI/constants/tableCollateral';
-import { MODAL_TABS } from '@/UI/constants/tabs';
 import { TABLET_BREAKPOINT } from '@/UI/constants/breakpoints';
 
 // Store
 import { useAppStore } from '@/UI/lib/zustand/store';
-
-// Utils
-import { getNumberValue } from '@/UI/utils/Numbers';
 
 // Hooks
 import useMediaQuery from '@/UI/hooks/useMediaQuery';
 import useToast from '@/UI/hooks/useToast';
 
 // Components
-import Balance from '@/UI/components/Balance/Balance';
 import Button from '@/UI/components/Button/Button';
-import DropdownMenu from '@/UI/components/DropdownMenu/DropdownMenu';
-import Tabs from '@/UI/components/Tabs/Tabs';
-import Input from '@/UI/components/Input/Input';
 import TableCollateral from '@/UI/components/TableCollateral/TableCollateral';
 import DisconnectedWallet from '@/UI/components/DisconnectedWallet/DisconnectedWallet';
 import Toast from '@/UI/components/Toast/Toast';
-
-const Modal = dynamic(() => import('@/UI/components/Modal/Modal'), {
-  ssr: false,
-});
-
+import ManageFundsModal from './ManageFundsModal';
 // Layouts
 import Flex from '@/UI/layouts/Flex/Flex';
 import Panel from '@/UI/layouts/Panel/Panel';
@@ -45,34 +33,28 @@ const CollateralPanel = () => {
   const tabletBreakpoint = useMediaQuery(TABLET_BREAKPOINT);
   const [collateralSummary, setCollateralSummary] = useState(TABLE_COLLATERAL_SUMMARY);
   const [selectedCurrency, setSelectedCurrency] = useState<{ name: string; value: string }>();
-  const [modalAmount, setModalAmount] = useState('');
   const [modalTab, setModalTab] = useState<string>('deposit');
-  const [transactionInProgress, setTransactionInProgress] = useState(false);
-
   const { toastList, position, showToast } = useToast();
+
+  const handleSuccess = (item: 'WETH' | 'USDC') => (balance: FetchBalanceResult) => {
+    setCollateralSummary(summary => ({
+      ...summary,
+      [item]: { ...summary[item], walletBalance: balance.formatted },
+    }));
+  };
 
   useBalance({
     address,
     token: systemInfo.tokenAddress['WETH'] as `0x${string}`,
     watch: true,
-    onSuccess(balanceWETH) {
-      setCollateralSummary(summary => ({
-        ...summary,
-        WETH: { ...summary['WETH'], walletBalance: balanceWETH.formatted },
-      }));
-    },
+    onSuccess: handleSuccess('WETH'),
   });
 
   useBalance({
     address,
     token: systemInfo.tokenAddress['USDC'] as `0x${string}`,
     watch: true,
-    onSuccess(balanceUSDC) {
-      setCollateralSummary(summary => ({
-        ...summary,
-        USDC: { ...summary['USDC'], walletBalance: balanceUSDC.formatted },
-      }));
-    },
+    onSuccess: handleSuccess('USDC'),
   });
 
   const fetchFundlockState = useCallback(async () => {
@@ -175,72 +157,14 @@ const CollateralPanel = () => {
         )}
         {!address && <DisconnectedWallet />}
       </Panel>
-      <Modal
-        title='Manage Funds'
+      <ManageFundsModal
+        selectedCurrency={selectedCurrency}
+        setSelectedCurrency={setSelectedCurrency}
         isOpen={!!selectedCurrency}
-        onCloseModal={() => setSelectedCurrency(undefined)}
-        isLoading={transactionInProgress}
-        onSubmitOrder={async () => {
-          if (!selectedCurrency) return;
-          setTransactionInProgress(true);
-          const currency = systemInfo.tokenAddress[selectedCurrency.value];
-          const amount = parseUnits(modalAmount, systemInfo.tokenDecimals[selectedCurrency.value]);
-
-          try {
-            if (modalTab === 'deposit') {
-              await ithacaSDK.fundlock.deposit(currency, amount);
-            } else {
-              await ithacaSDK.fundlock.withdraw(currency, amount);
-            }
-          } catch (error) {
-            console.log(`Failed to ${modalTab}`, error);
-          }
-          setModalAmount('');
-          setSelectedCurrency(undefined);
-          setTransactionInProgress(false);
-        }}
-      >
-        <Tabs tabs={MODAL_TABS} className='mb-20' activeTab={modalTab} onChange={setModalTab} />
-        <Flex direction='row-space-between' gap='gap-15'>
-          <DropdownMenu
-            className='full-width'
-            options={[
-              {
-                name: 'WETH',
-                value: 'WETH',
-              },
-              {
-                name: 'USDC',
-                value: 'USDC',
-              },
-            ]}
-            value={selectedCurrency}
-            onChange={option => setSelectedCurrency({ name: option, value: option })}
-            iconStart={selectedCurrency && collateralSummary[selectedCurrency.value].currencyLogo}
-          />
-          <Input
-            className='full-width'
-            containerClassName='full-width'
-            value={modalAmount}
-            onChange={({ target }) => setModalAmount(getNumberValue(target.value))}
-          />
-          <Button
-            variant='secondary'
-            size='sm'
-            title='Select All Assets'
-            onClick={() => selectedCurrency && setModalAmount(collateralSummary[selectedCurrency.value].walletBalance)}
-          >
-            All
-          </Button>
-        </Flex>
-        {selectedCurrency && (
-          <Balance
-            fundLock={collateralSummary[selectedCurrency.value].fundLockValue}
-            balance={collateralSummary[selectedCurrency.value].walletBalance}
-            margin='mtb-20'
-          />
-        )}
-      </Modal>
+        collateralSummary={collateralSummary}
+        modalTab={modalTab}
+        setModalTab={setModalTab}
+      />
     </>
   );
 };
